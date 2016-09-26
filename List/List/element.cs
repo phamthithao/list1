61e64ad6c6f358fbdadd8a94d456e93bd50cd22c
phamthithao <bunney322@gmail.com>
2016-09-26 09:22:06 +0700

    123

---
+++ List/List/element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class element<T>
+    {
+        private T data;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        element<T> next;
+
+        internal element<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+    }
+}
